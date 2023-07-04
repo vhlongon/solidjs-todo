27ab75a4856179daa1ffee4370b4530d60454a88
vhlongon <vhlongon@gmail.com>
2023-07-04 10:15:05 +0200

    keep props object in Todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TodoList } from './TodoList';
 
 const App = () => {
   return (
-    <div class="flex flex-col h-screen items-center justify-center">
+    <div class="flex flex-col min-h-screen p-2 items-center justify-center">
       <h1 class="text-lg font-bold mb-4">TODO APP</h1>
       <TodoList />
     </div>
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,30 +1,36 @@
-import { createSignal } from 'solid-js';
+import { createSignal, onCleanup, splitProps } from 'solid-js';
 import { deleteTodo, editTodo, toggleTodo } from './state';
 import { Todo as TodoProps } from './types';
+import clickOutside from './clickOutside';
 
-export const Todo = ({ done, id, text }: TodoProps) => {
-  const name = `${id}-checkbox`;
-  const [todoText, setTodoText] = createSignal(text);
+const outisdeClick = clickOutside;
+
+export const Todo = (props: TodoProps) => {
+  const name = `${props.id}-checkbox`;
+  const [todoText, setTodoText] = createSignal(props.text);
 
   return (
     <>
-      <form class="flex items-center justify-between my-2 p-2 gap-2 border border-gray-300 rounded">
+      <form
+        class="flex items-center justify-between my-2 p-2 gap-2 border border-gray-300 rounded"
+        use:clickOutside={() => console.log('clicked outside')}
+      >
         <div class="flex items-center">
           <input
             id={name}
             type="checkbox"
             value=""
             class="w-5 h-5 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2"
-            checked={done}
-            onClick={() => toggleTodo(id)}
-            onkeydown={e => e.key === ' ' && toggleTodo(id)}
+            checked={props.done}
+            onClick={() => toggleTodo(props.id)}
+            onkeydown={e => e.key === ' ' && toggleTodo(props.id)}
           />
           <label for={name} class="ml-2 text-sm font-medium">
             <input
               type="text"
               value={todoText()}
               onBlur={() => {
-                editTodo(id, todoText());
+                editTodo(props.id, todoText());
               }}
               onInput={e => setTodoText(e.target.value)}
               class="border-none outline-none bg-transparent"
@@ -33,7 +39,7 @@ export const Todo = ({ done, id, text }: TodoProps) => {
         </div>
         <button
           type="button"
-          onClick={() => deleteTodo(id)}
+          onClick={() => deleteTodo(props.id)}
           class="flex items-center justify-center justify-items-center w-5 h-5 rounded bg-red-500 text-white hover:bg-red-600"
           tabIndex={0}
         >
